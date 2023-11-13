27c57e9dc208faa0f5e1134d071c9611bb21fe89
ThomasFBN <thni0004@stud.kea.dk>
2023-11-13 14:00:38 +0100

    test eat metode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Animal animal2 = new Cat();
         animal1.makeSound();
         animal2.makeSound();
+        animal1.eat("Mad");
+        animal2.eat("mad");
 
 
     }
